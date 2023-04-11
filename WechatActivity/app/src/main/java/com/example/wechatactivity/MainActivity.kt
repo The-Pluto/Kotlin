014d18e0317640a70b0803c611bd68a193e727e6
liangcshao <2206884457@qq.com>
2023-04-11 23:00:26 +0800

    完成了Fragment的测试以及使用

---
+++ WechatActivity/app/src/main/java/com/example/wechatactivity/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.wechatactivity
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class MainActivity:AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button:Button = findViewById(R.id.button1)
+        button.setOnClickListener{
+            replaceFragment(AnotherRightFragment())
+        }
+        replaceFragment(RightFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
